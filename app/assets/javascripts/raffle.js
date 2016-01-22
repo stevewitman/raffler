746d6a6ea171971ec765c5731b9cfbab3b49c62d
Steve Witman <stevewitman@gmail.com>
2016-01-22 04:02:41 -0700

    added draw random winner functionality, and only draw from non-winners.

--- app/assets/javascripts/raffle.js
+++ app/assets/javascripts/raffle.js
@@ -10,9 +10,24 @@ myApp.controller('RaffleCtrl', ['$scope', function($scope) {
       name: "Nicole"
     }
   ];
+
   $scope.addEntry = function() {
   	$scope.entries.push($scope.newEntry);
   	$scope.newEntry = {};
-  }
+  };
+
+  $scope.drawWinner = function() {
+  	pool = [];
+  	angular.forEach($scope.entries, function(entry) {
+  		if (!entry.winner) {
+  		 	this.push(entry)
+  		}
+  	}, pool);
+  	if (pool.length > 0) {
+	  	entry = pool[Math.floor(Math.random()*pool.length)];
+	  	entry.winner = true;
+	  	$scope.lastWinner = entry
+  	}
+  };
 
 }]);
